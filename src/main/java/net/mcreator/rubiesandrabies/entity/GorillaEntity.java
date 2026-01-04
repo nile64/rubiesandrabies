542ee98e44d129179117aeb9625d1810e3cc80c0
poqqerss <golde@DESKTOP-4JLAAEQ.lan>
2026-01-03 20:18:57 -0500

    monkey.

--- src/main/java/net/mcreator/rubiesandrabies/entity/GorillaEntity.java
+++ src/main/java/net/mcreator/rubiesandrabies/entity/GorillaEntity.java
@@ -1,43 +1,59 @@
 package net.mcreator.rubiesandrabies.entity;
 
-import net.minecraft.nbt.Tag;
-import net.minecraft.network.syncher.EntityDataAccessor;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
+
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.rubiesandrabies.init.RubiesandrabiesModItems;
+import net.mcreator.rubiesandrabies.init.RubiesandrabiesModEntities;
 
 public class GorillaEntity extends Animal {
-
 	public final AnimationState animationState0 = new AnimationState();
 
 	public GorillaEntity(EntityType<GorillaEntity> type, Level world) {
 		super(type, world);
 		xpReward = 10;
 		setNoAi(false);
-
 	}
 
 	@Override
 	protected void registerGoals() {
 		super.registerGoals();
-
 		this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 2, false) {
-
 			@Override
 			protected boolean canPerformAttack(LivingEntity entity) {
 				return this.isTimeToAttack() && this.mob.distanceToSqr(entity) < (this.mob.getBbWidth() * this.mob.getBbWidth() + entity.getBbWidth()) && this.mob.getSensing().hasLineOfSight(entity);
 			}
-
 		});
 		this.targetSelector.addGoal(2, new HurtByTargetGoal(this).setAlertOthers());
 		this.goalSelector.addGoal(3, new BreedGoal(this, 1));
-		this.goalSelector.addGoal(4, new TemptGoal(this, 1, Ingredient.of(RubiesandrabiesModItems.DELETED_MOD_ELEMENT.get()), false));
+		this.goalSelector.addGoal(4, new TemptGoal(this, 1, Ingredient.of(RubiesandrabiesModItems.BANANA.get()), false));
 		this.goalSelector.addGoal(5, new RandomStrollGoal(this, 1));
 		this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
 		this.goalSelector.addGoal(7, new FloatGoal(this));
-
 	}
 
 	protected void dropCustomDeathLoot(ServerLevel serverLevel, DamageSource source, boolean recentlyHitIn) {
 		super.dropCustomDeathLoot(serverLevel, source, recentlyHitIn);
-		this.spawnAtLocation(serverLevel, new ItemStack(RubiesandrabiesModItems.DELETED_MOD_ELEMENT.get()));
+		this.spawnAtLocation(serverLevel, new ItemStack(RubiesandrabiesModItems.BANANA.get()));
 	}
 
 	@Override
@@ -65,7 +81,6 @@ public class GorillaEntity extends Animal {
 	@Override
 	public void tick() {
 		super.tick();
-
 		if (this.level().isClientSide()) {
 			this.animationState0.animateWhen(true, this.tickCount);
 		}
@@ -80,7 +95,7 @@ public class GorillaEntity extends Animal {
 
 	@Override
 	public boolean isFood(ItemStack stack) {
-		return Ingredient.of(RubiesandrabiesModItems.DELETED_MOD_ELEMENT.get()).test(stack);
+		return Ingredient.of(RubiesandrabiesModItems.BANANA.get()).test(stack);
 	}
 
 	public static void init(RegisterSpawnPlacementsEvent event) {
@@ -95,16 +110,10 @@ public class GorillaEntity extends Animal {
 		builder = builder.add(Attributes.ARMOR, 5);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 10);
 		builder = builder.add(Attributes.FOLLOW_RANGE, 16);
-
 		builder = builder.add(Attributes.STEP_HEIGHT, 0.6);
-
 		builder = builder.add(Attributes.KNOCKBACK_RESISTANCE, 0.5);
-
 		builder = builder.add(Attributes.ATTACK_KNOCKBACK, 3);
-
 		builder = builder.add(Attributes.TEMPT_RANGE, 10);
-
 		return builder;
 	}
-
 }
