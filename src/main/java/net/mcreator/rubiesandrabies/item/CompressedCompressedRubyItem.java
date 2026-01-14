ca1b0c3880e714d43a73a2080219f40aef950efc
CoolEnder32 <hellowillrowan@gmail.com>
2026-01-14 14:47:28 -0500

    tweaks

--- src/main/java/net/mcreator/rubiesandrabies/item/CompressedCompressedRubyItem.java
+++ src/main/java/net/mcreator/rubiesandrabies/item/CompressedCompressedRubyItem.java
@@ -1,5 +1,9 @@
 package net.mcreator.rubiesandrabies.item;
 
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+
 public class CompressedCompressedRubyItem extends Item {
 	public CompressedCompressedRubyItem(Item.Properties properties) {
 		super(properties.rarity(Rarity.UNCOMMON));
--- src/main/java/net/mcreator/rubiesandrabies/item/ZorpiniumRodItem.java
+++ src/main/java/net/mcreator/rubiesandrabies/item/ZorpiniumRodItem.java
@@ -1,5 +1,8 @@
 package net.mcreator.rubiesandrabies.item;
 
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
 public class ZorpiniumRodItem extends Item {
 	public ZorpiniumRodItem(Item.Properties properties) {
 		super(properties.rarity(Rarity.EPIC).fireResistant());
