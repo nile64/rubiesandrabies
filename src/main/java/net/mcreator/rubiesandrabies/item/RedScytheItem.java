e5dccd828fbfda1d0f0b3fb76d6603eb295555c2
therealKoolant <73361781+therealKoolant@users.noreply.github.com>
2026-01-21 15:47:06 -0500

    ev

--- src/main/java/net/mcreator/rubiesandrabies/item/RedScytheItem.java
+++ src/main/java/net/mcreator/rubiesandrabies/item/RedScytheItem.java
@@ -8,9 +8,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.Registries;
 
 public class RedScytheItem extends Item {
-	private static final ToolMaterial TOOL_MATERIAL = new ToolMaterial(BlockTags.INCORRECT_FOR_WOODEN_TOOL, 4096, 10f, 0, 5, TagKey.create(Registries.ITEM, ResourceLocation.parse("rubiesandrabies:red_scythe_repair_items")));
+	private static final ToolMaterial TOOL_MATERIAL = new ToolMaterial(BlockTags.INCORRECT_FOR_WOODEN_TOOL, 4096, 9.5f, 0, 5, TagKey.create(Registries.ITEM, ResourceLocation.parse("rubiesandrabies:red_scythe_repair_items")));
 
 	public RedScytheItem(Item.Properties properties) {
-		super(properties.sword(TOOL_MATERIAL, 6f, -2f));
+		super(properties.sword(TOOL_MATERIAL, 6.5f, -2f));
 	}
 }
