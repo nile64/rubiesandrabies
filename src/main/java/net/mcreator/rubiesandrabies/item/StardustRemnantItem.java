e4b8aca6977aa9585e366a82a2a6b020ae3da4c2
CoolEnder32 <hellowillrowan@gmail.com>
2026-01-21 15:26:53 -0500

    sync

--- src/main/java/net/mcreator/rubiesandrabies/item/StardustRemnantItem.java
+++ src/main/java/net/mcreator/rubiesandrabies/item/StardustRemnantItem.java
@@ -1,5 +1,8 @@
 package net.mcreator.rubiesandrabies.item;
 
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
 public class StardustRemnantItem extends Item {
 	public StardustRemnantItem(Item.Properties properties) {
 		super(properties.rarity(Rarity.UNCOMMON).fireResistant());
--- src/main/java/net/mcreator/rubiesandrabies/procedures/RacismPlanetPlayerEntersDimensionProcedure.java
+++ src/main/java/net/mcreator/rubiesandrabies/procedures/RacismPlanetPlayerEntersDimensionProcedure.java
@@ -1,6 +1,21 @@
 package net.mcreator.rubiesandrabies.procedures;
 
+import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
+import net.neoforged.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.rubiesandrabies.client.RenderUtils;
+
+import javax.annotation.Nullable;
 
 @EventBusSubscriber(Dist.CLIENT)
 public class RacismPlanetPlayerEntersDimensionProcedure {
